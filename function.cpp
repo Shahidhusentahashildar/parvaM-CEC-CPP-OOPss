9727ac358c58f75d9a5c19b829792bf14b2a1d5b
Shahidhusentahashildar <shahidhusentahashildar@gmail.com>
2025-05-22 16:22:55 +0530

    Add files via upload

--- function.cpp
+++ function.cpp
@@ -3,21 +3,15 @@ using namespace std;
 
 void sayHello(){
     cout<<"Hello "<<endl;
-
 }
 int sayHi(string name){  //function wth parameters 
     cout<<"Hi,"<<name<<endl;
     return 0;
 }
-
-
 int greet(string fname,string lname,int age){  //function wth parameters 
     cout<<"Hi,"<<fname<< " "<< lname <<",Age:"<<age <<endl;
     return 0;
 }
-
-
-
 int main(){
     string firstName ,lastname ,friendName ;
     int Age;
